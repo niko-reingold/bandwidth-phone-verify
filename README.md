bd8ef7cd534a49d1b41e2fdd6c53689c539dbaee
Niko Reingold <nreingold@nreingold-air.local>
2016-07-13 15:10:57 -0400

    commit to Github

---
+++ README.md
@@ -0,0 +1,25 @@
+#Bandwidth Python Demo
+
+This is a basic number verification app made from modifying heroku's getting started program.  It utilizes the flask web framework.
+
+[![Deploy](https://www.herokucdn.com/deploy/button.png)](https://heroku.com/deploy)
+
+## Documentation
+
+For more information about using Python on Heroku, see these Dev Center articles:
+
+[![Deploy to Heroku](https://www.herokucdn.com/deploy/button.png)](https://heroku.com/deploy)
+
+## Install / Configuration
+Before installation you must have the following:
+* A Bandwidth Application Platform account ([sign up here](https://catapult.inetwork.com/pages/signup.jsf))
+* Your App Platform userId, apiToken and apiSecret ([see here](http://ap.bandwidth.com/docs/security/))
+
+## 1-Click Deploy to Heroku
+You'll need a heroku account and to also set the environment variables.
+* BANDWIDTH_USER_ID (found on the account tab of the developer console)
+* BANDWIDTH_API_TOKEN (found on the account tab of the developer console)
+* BANDWIDTH_API_SECRET (found on the account tab of the developer console)
+* PHONE_NUMBER (Bandwidth number associated with your app on the my apps tab)
+
+These environment variables can be set in the settings tab of the Heroku dashboard, under Config Variables.
---
+++ app.py
@@ -0,0 +1,72 @@
+from flask import Flask
+from flask import render_template
+from flask import request
+from bandwidth_sdk import Client
+from bandwidth_sdk import Message
+from bandwidth_sdk import Call
+import logging
+import random
+import os
+
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+
+app = Flask(__name__)
+
+Client(os.environ['BANDWIDTH_USER_ID'], os.environ['BANDWIDTH_API_TOKEN'], os.environ['BANDWIDTH_API_SECRET'])
+fromNumber = os.environ['PHONE_NUMBER']
+
+code = random.randint(1000, 9999)
+
+
+@app.route('/', methods=['GET'])
+def main_page():
+    return render_template('index.html')
+
+
+@app.route('/verify', methods=['GET'])
+def verify_page():
+    global code
+    num = request.args.get('number')
+    if num[:2] != '+1':
+        num = '+1' + num
+    code = random.randint(1000, 9999)
+    logging.debug(code)
+    sendCode(num, request.args.get('action'), code)
+    logging.debug(code)
+    return render_template('verify.html')
+
+
+@app.route('/result', methods=['GET'])
+def result_page():
+    success = verifyCode(request.args.get('input'))
+    return render_template('result.html', success=success)
+
+
+@app.route('/callEvents', methods=['GET'])
+def speakCode():
+    if request.args.get('eventType') == 'answer':
+        logging.debug('<Response>' + '<SpeakSentence voice="kate" locale="en_US" gender="female">' + str(
+            request.args.get('tag')) + '</SpeakSentence>' + '<Hangup></Hangup>' + '</Response>')
+        return '<Response>' + '<SpeakSentence voice="kate" locale="en_US" gender="female">' + str(
+            request.args.get('tag')) + '</SpeakSentence>' + '<Hangup></Hangup>' + '</Response>'
+
+
+def sendCode(number, method, code):
+    logging.debug(code)
+    if method == 'Call':
+        host = 'https://' + request.headers.get('Host') + '/callEvents'
+        Call.create(fromNumber, number, callback_url=host, callbackHttpMethod='GET', tag=code)
+    else:
+        Message.send(sender=fromNumber, receiver=number, text=code, tag='verify Code')
+
+
+def verifyCode(input):
+    global code
+    logging.debug(input)
+    logging.debug(code)
+    return int(input) == code
+
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
